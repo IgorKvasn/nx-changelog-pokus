4f0f5ecc0ae16bfcfb2abe5c06364d86d2403f11
Igor Kvasnicka <igor.kvasnicka@annotation.sk>
2022-01-01 11:34:30 +0100

    feat!: braking BREAKING CHANGE: aaa

--- libs/lib1/src/lib/lib1.tsx
+++ libs/lib1/src/lib/lib1.tsx
@@ -7,7 +7,7 @@ export interface Lib1Props {}
 export function Lib1(props: Lib1Props) {
   return (
     <div>
-      <h1>Welcome to Lib1! 22222</h1>
+      <h1>Welcome to Lib1! 222223</h1>
       <Lib2/>
     </div>
   );
