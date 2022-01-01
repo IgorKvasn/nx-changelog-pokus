76bbd363be3c01ee392216496c892da36c02f835
Igor Kvasnicka <igor.kvasnicka@annotation.sk>
2022-01-01 11:10:52 +0100

    feat: initial commit

---
+++ libs/lib1/src/lib/lib1.tsx
@@ -0,0 +1,14 @@
+import './lib1.module.scss';
+
+/* eslint-disable-next-line */
+export interface Lib1Props {}
+
+export function Lib1(props: Lib1Props) {
+  return (
+    <div>
+      <h1>Welcome to Lib1!</h1>
+    </div>
+  );
+}
+
+export default Lib1;
---
+++ libs/lib2/src/lib/lib2.tsx
@@ -0,0 +1,14 @@
+import './lib2.module.scss';
+
+/* eslint-disable-next-line */
+export interface Lib2Props {}
+
+export function Lib2(props: Lib2Props) {
+  return (
+    <div>
+      <h1>Welcome to Lib2!</h1>
+    </div>
+  );
+}
+
+export default Lib2;
